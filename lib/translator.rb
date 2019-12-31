aeafef4f3e29294e962db97baadc89bed4db4193
Simon Tucker <sjtucker28@gmail.com>
2019-12-31 14:22:26 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,7 +14,7 @@ def load_library(file_path)
 end
 
 def get_japanese_emoticon(file_path, emoticon)
-  load_library(file_path)
+  library = load_library(file_path)
   if library["get_emoticon"].include?(emoticon)
     return library["get_emoticon"][emoticon]
   else
